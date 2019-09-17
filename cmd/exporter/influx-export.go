fff811f61a7d2af50c7ef2204167addae4ecc155
pavel m <paullus325@gmail.com>
2019-09-17 15:34:08 +0300

    influx

--- cmd/exporter/influx-export.go
+++ cmd/exporter/influx-export.go
@@ -6,7 +6,7 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/influxdata/influxdb/client/v2"
+	"github.com/influxdata/influxdb1-client/v2"
 	"github.com/sevlyar/go-daemon"
 
 	"github.com/m-pavel/go-apogee/lib"
