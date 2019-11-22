40111a5ec6af50309b78fbf09de3ce12fb5fbfc5
pavel m <paullus325@gmail.com>
2019-11-22 17:55:51 +0300

    some configuration

--- cmd/mqtt/mqtt.go
+++ cmd/mqtt/mqtt.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"github.com/m-pavel/go-apogee/lib"
 	"github.com/m-pavel/go-hassio-mqtt/pkg"
@@ -9,18 +10,25 @@ import (
 )
 
 type ApogeeService struct {
-	a *apogee.Apogee
+	a         *apogee.Apogee
+	lightType *string
 }
 type Request struct {
 	Sun float32 `json:"sun"`
 }
 
-func (ts ApogeeService) PrepareCommandLineParams() {}
-func (ts ApogeeService) Name() string              { return "apogee" }
+func (ts *ApogeeService) PrepareCommandLineParams() {
+	ts.lightType = flag.String("light", "sun", "sun or electric")
+}
+func (ts ApogeeService) Name() string { return "apogee" }
 
 func (ts *ApogeeService) Init(client MQTT.Client, topic, topicc, topica string, debug bool, ss ghm.SendState) error {
+	lt := apogee.Sunlight
+	if "electric" == *ts.lightType {
+		lt = apogee.Electric
+	}
 	var err error
-	ts.a, err = apogee.FindUsbOne(&apogee.LibUsbFct{LightType: apogee.Sunlight, Debug: false})
+	ts.a, err = apogee.FindUsbOne(&apogee.LibUsbFct{LightType: lt, Debug: debug})
 	return err
 }
 
